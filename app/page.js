ca2440883e87b82c822ac1c71230779f042ba2af
preetamimg <preetam.img@gmail.com>
2025-02-27 20:21:57 +0530

    home

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <div className="h-dvh d-flex align-items-center justify-content-center">
         <h1>Home Page</h1>
+        <Link href="/login">Login</Link>
       </div>
     </>
   );
